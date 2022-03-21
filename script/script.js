5d47985c8047b6a21754c46ab8a90b859156cd1d
giorgios17 <giosur@live.it>
2022-03-21 14:01:37 +0100

    array of members

---
+++ script/script.js
@@ -0,0 +1,31 @@
+//Aggiungere un file js in cui definire *un array di oggetti* che rappresentano i membri del team. 
+//Ogni membro ha le informazioni necessarie per stampare la relativa card: *Nome, Ruolo e Foto*.
+//Una volta definita la struttura dati, prendendo come riferimento 
+//la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
+
+const members = [
+    {nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'img/wayne-barnett-founder-ceo.jpg'
+    },
+    {nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto: 'img/angela-caroll-chief-editor.jpg'
+    },
+    {nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: 'img/walter-gordon-office-manager.jpg'
+    },
+    {nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: 'img/scott-estrada-developer.jpg'
+    },
+    {nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto: 'img/barbara-ramos-graphic-designer.jpg'
+    }
+];
